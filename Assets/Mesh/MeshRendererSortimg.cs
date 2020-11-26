0dcf9b947ce1fee6562faa61c1e10afebe5cc455
tsaiyuyes7 <39110981+tsaiyuyes7@users.noreply.github.com>
2020-11-27 00:08:12 +0800

    11/26

---
+++ Assets/Mesh/MeshRendererSortimg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRendererSortimg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private MeshRenderer meshRenderer;
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.sortingLayerName = "player";
+        meshRenderer.sortingOrder = 3;
+    }
+
+}
